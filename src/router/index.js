dc815034e4c4e7a2a1cd0acd3daeac366b5bc08a
Marco <marrco.gonzalez@gmail.com>
2024-11-05 11:52:37 -0300

    index.js update

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+//import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import HomeView from '@/views/HomeView.vue'; 
 import AddTask from '@/views/AddTask.vue';
 import TaskList from '@/views/TaskList.vue';
 import CombinedView from '@/views/CombinedView.vue';
-
+/*
 Vue.use(Router);
 
 export default new Router({
@@ -30,4 +31,39 @@ export default new Router({
       component: CombinedView
     },
   ]
+
+  
+});*/
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomeView',
+    component: HomeView,
+  },
+  {
+    path: '/addtask',
+    name: 'AddTask',
+    component: AddTask
+  },
+  {
+    path: '/tasklist',
+    name: 'TaskList',
+    component: TaskList
+  },
+  {
+    path: '/combined',
+    name: 'CombinedView',
+    component: CombinedView
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 });
+
+export default router;
